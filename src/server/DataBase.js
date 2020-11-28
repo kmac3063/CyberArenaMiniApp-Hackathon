d5a1786a584983356adc97ca0600491c6a896b70
BLGALEX <lepehaleha@bk.ru>
2020-11-28 16:26:57 +1000

    Добавил шаблон функции для запросана на сервер по вкладке турниры

--- src/server/DataBase.js
+++ src/server/DataBase.js
@@ -1,12 +1,13 @@
 import paladinsImg from '../img/paladins.png';
 import dotaImg from '../img/dota.png';
 import steamImg from '../img/steam.png';
+import defHS from '../img/defaultHS.png';
 import vkImg from '../img/vk.png';
 import React from 'react';
 
+// если не получили, то создать
 class DataBase {
     static getUserInfo(fetchUser) {
-        // если не получили, то создать
         //запрос к бд
         let props = {};
         props.games = ["Paladins", "Dota 2"];
@@ -26,6 +27,42 @@ class DataBase {
         // запрос к бд
         let tmp = {"Vk" : vkImg, "Steam" : steamImg};
         return tmp.get(name);
+    }
+
+    static getUserTournamentsInfo(fetchUser){
+        //запрос к бд
+        let props = {};
+        props.myTournaments = [];
+        props.createdTournaments = [];
+        let tournament1 = {
+            "Name": "Первый турниро по хс",
+            "StartData": "28.01.2020 г.",
+            "StartTime": "15:30",
+            "EndData": null,
+            "EndTime": null,
+            "GameType": "Hearthstone",
+            "Prize": null,
+            "EnterPrice": null,
+            "TournamentImg": defHS,
+            "PrivateType": "private"
+        }
+
+        let tournament2 = {
+            "Name": "Второй турниро по хс",
+            "StartData": "29.01.2020 г.",
+            "StartTime": "16:30",
+            "EndData": "30.01.2020 г.",
+            "EndTime": null,
+            "GameType": "Hearthstone",
+            "Prize": 150000,
+            "EnterPrice": null,
+            "TournamentImg": defHS,
+            "PrivateType": "public"
+        }
+        props.myTournaments.push(tournament1, tournament2);
+        props.createdTournaments.push(tournament1);
+        props.recomendedTournaments = [];
+        return props;
     }
 }
 
--- src/tabs/Tournament.js
+++ src/tabs/Tournament.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import Panel from "@vkontakte/vkui/dist/components/Panel/Panel";
 import Group from "@vkontakte/vkui/dist/components/Group/Group";
+import Title from "@vkontakte/vkui/dist/components/Typography/Title/Title";
+import Button from "@vkontakte/vkui/dist/components/Button/Button";
+import Avatar from "@vkontakte/vkui/dist/components/Avatar/Avatar";
+import RichCell from "@vkontakte/vkui/dist/components/RichCell/RichCell";
+import DataBase from "../server/DataBase"
+import Icon28WriteSquareOutline from '@vkontakte/icons/dist/28/write_square_outline';
 
-const Tournament = props => {
-
+const Tournament = (props) => {
+    let user = DataBase.getUserTournamentsInfo(props.fetchedUser);
     return (<Group>
             <div>Турнир</div>
         </Group>)
