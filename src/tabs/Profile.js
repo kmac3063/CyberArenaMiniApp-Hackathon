de0a1e1b7c6cf1b4d5700437877281910ff8ee6b
kmac3063 <lancovlancov@mail.ru>
2020-11-28 16:28:33 +1000

    Сделано модальное окно в профиле, добавлены карты

--- src/tabs/Profile.js
+++ src/tabs/Profile.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Panel from "@vkontakte/vkui/dist/components/Panel/Panel";
 import Group from "@vkontakte/vkui/dist/components/Group/Group";
 import Title from "@vkontakte/vkui/dist/components/Typography/Title/Title";
@@ -7,9 +7,25 @@ import Avatar from "@vkontakte/vkui/dist/components/Avatar/Avatar";
 import RichCell from "@vkontakte/vkui/dist/components/RichCell/RichCell";
 import DataBase from "../server/DataBase"
 import Icon28WriteSquareOutline from '@vkontakte/icons/dist/28/write_square_outline';
+import EditProfile from "../modals/EditProfile";
+import {IS_PLATFORM_ANDROID, IS_PLATFORM_IOS, ModalPage, ModalRoot} from "@vkontakte/vkui";
+import ModalPageHeader from "@vkontakte/vkui/dist/components/ModalPageHeader/ModalPageHeader";
+import PanelHeaderButton from "@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton";
+import FormLayout from "@vkontakte/vkui/dist/components/FormLayout/FormLayout";
+import Textarea from "@vkontakte/vkui/dist/components/Textarea/Textarea";
+import Icon24Done from '@vkontakte/icons/dist/24/done';
+import Icon24Cancel from '@vkontakte/icons/dist/24/cancel';
+import CardGrid from "@vkontakte/vkui/dist/components/CardGrid/CardGrid";
+import Card from "@vkontakte/vkui/dist/components/Card/Card";
 
 const Profile = (props) => {
+    const [activeModal, setActiveModal] = useState(null);
     let user = DataBase.getUserInfo(props.fetchedUser);
+
+    const save = () => {
+        console.log("save");
+        setActiveModal(null);
+    }
     return (<Group>
         <Title level="2" weight="semibold" style={{"textAlign" : "center"}}>Основная информация</Title>
         {props.fetchedUser && <RichCell
@@ -18,11 +34,37 @@ const Profile = (props) => {
             before={<Avatar size={72} src={user.avatar} />}
             text={props.fetchedUser.city.title}
            //caption={props.fetchedUser.city.title}
-            after={<Button before={<Icon28WriteSquareOutline/>} size="l" onClick={() => <editProfile/>}/>}
-        >
-            {user.nickname}
-        </RichCell>}
-        <div>Profile</div>
+            after={<Icon28WriteSquareOutline onClick={()=>{setActiveModal("editProfile")}}/>}>
+                    {user.nickname}
+                </RichCell>}
+        <ModalRoot activeModal={activeModal}>
+            <ModalPage
+                id={"editProfile"}
+                onClose={() => setActiveModal(null)}
+                header={
+                    <ModalPageHeader
+                        left={IS_PLATFORM_ANDROID && <PanelHeaderButton onClick={save}><Icon24Cancel /></PanelHeaderButton>}
+                        right={<PanelHeaderButton onClick={save}>{IS_PLATFORM_IOS ? 'Готово' : <Icon24Done />}</PanelHeaderButton>}
+                    >
+                        Редактирование
+                    </ModalPageHeader>
+                }
+            >
+                <FormLayout>
+                    <Textarea top="Новый никнейм" placeholder="Введите новый никнейм" />
+                </FormLayout>
+            </ModalPage>
+        </ModalRoot>
+        <CardGrid>
+            <Card size="l" mode="shadow">
+                <div style={{ height: 96 }} />
+            </Card>
+        </CardGrid>
+        <CardGrid>
+            <Card size="l" mode="shadow">
+                <div style={{ height: 96 }} />
+            </Card>
+        </CardGrid>
     </Group>)
 }
 
