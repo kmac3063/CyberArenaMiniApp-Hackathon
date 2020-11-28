875d24668ba754b5b68612dfb2456ff70fb850e8
BLGALEX <lepehaleha@bk.ru>
2020-11-29 04:46:34 +1000

    Добавлены панели

--- src/OrgTourView.js
+++ src/OrgTourView.js
@@ -1,9 +1,15 @@
 import React, {useState} from 'react';
 import Group from "@vkontakte/vkui/dist/components/Group/Group";
+import PanelHeader from "@vkontakte/vkui/dist/components/PanelHeader/PanelHeader";
+import {PanelHeaderBack} from "@vkontakte/vkui";
 
 const OrgTourView = (props) => {
 
-    return <Group>a</Group>
+    return <Group>
+        <PanelHeader separator={false} left={<PanelHeaderBack onClick={() =>{props.changePanel("panel1")}}/>}>
+            123
+        </PanelHeader>
+    </Group>
 }
 
 export default OrgTourView
