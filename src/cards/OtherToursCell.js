9e62367fae36bae2615eaf537b94464715b4191e
BLGALEX <lepehaleha@bk.ru>
2020-11-29 03:39:47 +1000

    Переделана вкладка с турнирами

--- src/cards/OtherToursCell.js
+++ src/cards/OtherToursCell.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react';
 import Avatar from "@vkontakte/vkui/dist/components/Avatar/Avatar";
 import Icon24Cancel from '@vkontakte/icons/dist/24/cancel';
 import PanelHeaderButton from "@vkontakte/vkui/dist/components/PanelHeaderButton/PanelHeaderButton";
@@ -6,72 +7,21 @@ import RichCell from "@vkontakte/vkui/dist/components/RichCell/RichCell";
 import SimpleCell from "@vkontakte/vkui/dist/components/SimpleCell/SimpleCell";
 import Text from "@vkontakte/vkui/dist/components/Typography/Text/Text";
 import Group from "@vkontakte/vkui/dist/components/Group/Group";
-import Card from "@vkontakte/vkui/dist/components/Card/Card";
 import defHS from "../img/defaultHS.png";
-import React from "react";
+import Card from "@vkontakte/vkui/dist/components/Card/Card";
+import Banner from "@vkontakte/vkui/dist/components/Banner/Banner";
+import Button from "@vkontakte/vkui/dist/components/Button/Button";
 
 const OtherToursCell = (props) => {
 
-
     return (
-        <Card size="m" mode="shadow" >
-            <div style={{
-                height: 228,
-                width: 448
-            }} >
-                <table style={{display:'flex',width : '100%', height: '50%',
-                }}>
-                    <img src={defHS} width={"100%"} align={"Center"} />
-                </table>
-                <table style={{width : '100%'}}>
-                    <tr style={{width : '100%'}}>
-                        <td style={{
-                            width: '50%',
-                            color: 'Green',
-                            textAlign: 'center'
-                        }} >
-                            {(props.title["StartData"] != null) ? 'Начало: '+props.title["StartData"] : ''}
-                            {(props.title["StartTime"] != null) ? ' ' + props.title["StartTime"] : ''}
-                        </td>
-                        <td style={{
-                            width: '50%',
-                            color: 'Red',
-                            textAlign: 'center'
-                        }}>
-                            {(props.title["EndData"] != null) ? 'Окончание: ' + props.title["EndData"] : ''}
-                            {(props.title["EndData"] != null) ? ' ' + props.title["EndTime"] : ''}
-                        </td>
-                    </tr>
-                </table>
-                <table style={{width : '100%'}}>
-                    <tr style={{width: '100%'}}>
-                        <td style={{
-                            width: '100%',
-                            textAlign: 'center',
-                            fontSize: 'large'
-                        }}>
-                            {props.title["Name"]}
-                        </td>
-                    </tr>
-                </table>
-                <table style={{width : '100%'}}>
-                    <tr style={{width : '100%'}}>
-                        <td style={{
-                            width: '50%',
-                            textAlign: 'center'
-                        }} >
-                            {(props.title["Prize"] != null) ? 'Призовой фонд: ' + props.title["Prize"] : ''}
-                        </td>
-                        <td style={{
-                            width: '50%',
-                            textAlign: 'center'
-                        }}>
-                            {(props.title["EnterPrice"] != null) ? 'Вход: ' + props.title["EnterPrice"] : ''}
-                        </td>
-                    </tr>
-                </table>
-            </div>
-        </Card>
+        <Banner
+            size = "m"
+            before={<Avatar size={112} mode="image" src={props.title["imgUrl"]} />}
+            header={props.title["Name"]}
+            subheader={props.title["GameType"]}
+            actions={<Button>Подробнее</Button>}
+        />
     )
 }
 
