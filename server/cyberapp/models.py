52c2552e16466942eaeb16bfe477614431ca7663
ephemeral sadness <49094121+ephemeralsadness@users.noreply.github.com>
2020-11-29 14:55:04 +1000

    Сервер закончен

--- server/cyberapp/models.py
+++ server/cyberapp/models.py
@@ -2,30 +2,42 @@ from django.db import models
 
 
 class Achievement(models.Model):
-    name = models.CharField(verbose_name='Name', max_length=64)
-
-
-class Contest(models.Model):
-    id = models.IntegerField(verbose_name='Id', primary_key=True)
-    name = models.CharField(verbose_name='Name', max_length=64)
-    type = models.IntegerField(verbose_name='Type')
-    is_private = models.BooleanField(verbose_name='Is_Private')
-    picture = models.ImageField(verbose_name='Image', upload_to='media/', null=True)
-    datetime = models.DateTimeField(verbose_name='Date_And_Tame', null=True)
-    prize = models.IntegerField(verbose_name='Prize', default=0)
-    manager_id = models.CharField(verbose_name='Manager_Id', max_length=16)
+    name = models.CharField(verbose_name='Name', max_length=64, default='Достижение')
 
 
 class Player(models.Model):
     vk_id = models.CharField(verbose_name='VK_Id', primary_key=True, max_length=16)
-    nickname = models.CharField(verbose_name='Nickname', max_length=64, null=True)
+    nickname = models.CharField(verbose_name='Nickname', max_length=64, default='')
     rating = models.IntegerField(verbose_name='Rating', default=0)
-    steam_profile = models.CharField(verbose_name='Steam_Profile', max_length=128, null=True)
-    battle_net_profile = models.CharField(verbose_name='Battle_net_Profile', max_length=128, null=True)
-    achievements = models.ManyToManyField(Achievement, blank=True)
-    contests = models.ManyToManyField(Contest, through='Participation')
+    steam_profile = models.CharField(verbose_name='Steam_Profile', max_length=128, default='')
+    battle_net_profile = models.CharField(verbose_name='Battle_net_Profile', max_length=128, default='')
+    achievements = models.ManyToManyField(Achievement)
+
+    def __str__(self):
+        return self.vk_id
 
 
-class Participation:
-    contest = models.ForeignKey(Contest, on_delete=models.CASCADE)
-    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+class Contest(models.Model):
+    id = models.IntegerField(verbose_name='Id', primary_key=True, unique=True, db_index=True)
+    game = models.CharField(verbose_name='Game', max_length=128, default="Название игры")
+    name = models.CharField(verbose_name='Name', max_length=128, default="Турнир")
+    datetime_begin = models.DateTimeField(verbose_name='Begin_Date', default="2000-01-01 00:00:00")
+    datetime_end = models.DateTimeField(verbose_name='End_Date', default="2000-01-02 00:00:00")
+    is_private = models.BooleanField(verbose_name='Is_Private', default=True)
+    description = models.CharField(verbose_name='Description', max_length=512, default="Описание турнира")
+    rules = models.CharField(verbose_name='Rules', max_length=512, default="Правила турнира")
+    team_limit = models.IntegerField(verbose_name='Team_Limit', default=16)
+    price = models.IntegerField(verbose_name='Price', default=0)
+    prize = models.IntegerField(verbose_name='Prize', default=0)
+    manager_id = models.CharField(verbose_name='VK_Id', max_length=16)
+    table_type = models.IntegerField(verbose_name='Table_Type', default=0)
+    table_data = models.CharField(verbose_name='Table_Data', default='', max_length=1024)
+    players = models.ManyToManyField(Player, verbose_name='Players', through='Participation')
+
+    def __str__(self):
+        return self.id
+
+
+class Participation(models.Model):
+    contests = models.ForeignKey(Contest, on_delete=models.CASCADE)
+    players = models.ForeignKey(Player, on_delete=models.CASCADE)
--- server/cyberapp/views.py
+++ server/cyberapp/views.py
@@ -1,36 +1,135 @@
 from django.shortcuts import render
+import rest_framework
+from rest_framework import generics
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from cyberapp.models import *
+from django.http import JsonResponse
+from django.forms.models import model_to_dict
+from cyberapp.id_generator import generate_id
+from django.core import serializers as core_serializers
 
-
-from rest_framework import generics
-from cyberapp.models import Player, Contest
 from rest_framework import serializers
 
 
-class TestCreateContestSerializer(serializers.ModelSerializer):
+class PlayerSerializer(serializers.Serializer):
     class Meta:
         model = Player
+        fields = 'vk_id'
+
+
+class ContestSerializer(serializers.Serializer):
+    player = PlayerSerializer(read_only=True, many=True)
+    class Meta:
+        model = Contest
         fields = '__all__'
 
 
-class TestCreateContestApiView(generics.CreateAPIView):
-    serializer_class = TestCreateContestSerializer
-    queryset = Player.objects.all()
+class TestContestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contest
+        fields = '__all__'
 
 
-class UserApiView(APIView):
+class TestAddContestAPIView(generics.CreateAPIView):
+    serializer_class = TestContestSerializer
+    queryset = Contest.objects.all()
 
-    # создание нового пользователя
-    # vk_id - идентификатор пользователя ВКонтакте (строка вида id1234...89)
-    #
+
+class TestRUDContestAPIView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = TestContestSerializer
+    queryset = Contest.objects.all()
+
+
+# get all public tournaments
+class AllContestsAPIView(APIView):
+    def get(self, request, format=None):
+        return JsonResponse(
+                list(Contest.objects.all().values()),
+                safe=False
+        )
+
+# vk_id -> all info
+# contest_id -> all info
+# post new user
+# post new contest
+
+
+class UserAPIView(APIView):
+    # .../?format=json&id=id12311222
     def post(self, request, format=None):
-        ...
+        p = Player(vk_id=request.GET.dict().get('id'))
+        print(request.GET.dict().get('id'))
+        p.save()
+
+        return JsonResponse({'done': True}, safe=False)
+
+    # .../?format=json&id=id12311222
+    def get(self, request, format=None):
+        d = model_to_dict(Player.objects.get(vk_id=request.GET.dict().get('id')))
+        return JsonResponse(
+                d,
+                safe=False
+        )
+
+
+class ContestAPIView(APIView):
+    # .../?format=json&id=id12311222
+    def post(self, request, format=None):
+        d = request.GET.dict()
+        i = generate_id()
+        c = Contest(
+            id=i,
+            game=d.get('game'),
+            name=d.get('name'),
+            datetime_begin=d.get('date_begin'),
+            datetime_end=d.get('date_end'),
+            rules=d.get('rules'),
+            is_private=(len(d.get('is_private')) == 4),
+            team_limit=int(d.get('team_limit')),
+            manager_id=d.get('manager_id'),
+            description=d.get('description'),
+        )
+        c.save()
+
+        print(model_to_dict(c))
+
+        return JsonResponse({'id': i}, safe=False)
+
+    # .../?format=json&id=0
+    def get(self, request, format=None):
+        d = model_to_dict(Contest.objects.get(id=request.GET.dict().get('id')))
+        for i in range(len(d['players'])):
+            d['players'][i] = getattr(d['players'][i], 'vk_id')
+
+        return JsonResponse(
+                d,
+                safe=False
+        )
+
+
+class PlayersContestsAPIView(APIView):
 
     def get(self, request, format=None):
-        return Response(str(request.GET), status=status.HTTP_200_OK)
+        vk_id = request.GET.dict().get('id')
+        lst = [Contest.objects.all()]
 
-    def put(self, request, format=None):
-        ...
+        for c in lst:
+            print(getattr(c, 'players'))
 
+
+        return JsonResponse(
+            "",
+            safe=False
+        )
+
+
+class ParticipationAPIView(APIView):
+    # .../?format=json&id=0&vk_id=id12321
+    def post(self, request, format=None):
+        d = request.GET.dict()
+        p = Participation(contests=Contest.objects.get(id=d.get('id')), players=Player.objects.get(vk_id=d.get('vk_id')))
+        p.save()
+
+        return JsonResponse(list(Participation.objects.all().values()), safe=False)
